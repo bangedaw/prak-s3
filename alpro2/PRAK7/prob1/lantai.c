b44acd29656dbae53d088e15d4d2f7b6723b3c8e
bangedaw <edwardrumahorbo1@gmail.com>
2025-10-30 14:38:45 +0700

    feat: 100 prob1

---
+++ alpro2/PRAK7/prob1/lantai.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// n = 4 -> 5
+// n = 5 -> 7
+// n = 6 -> 12
+
+
+int ukurLantai(int n){
+    if(n < 4){
+        return n;
+    } else if(n == 4 & n == 5){
+        return n + ukurLantai(n - 3);
+    } else {
+        return ukurLantai(n - 2) + ukurLantai(n - 1);
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", ukurLantai(n));
+    return 0;
+}
